1f44323e0ae77e2c97da3aa7c8a88f1e3dd2182d
Your Name <you@example.com>
2021-08-30 15:23:41 +0900

    articles.html step1

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask.templating import render_template
+from data import Articles
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
+# @ < 데코레이터 
+@app.route('/',methods=['GET','POST'])
+def index():
+    name="KIM"
+    return render_template('index.html',data = name)
+
+@app.route('/articles',methods=['GET','POST'])
+def articles():
+    list_data = Articles()
+    return render_template('articles.html',data = list_data)
+
 if __name__ == '__main__':
-    app.run(port=8000 , debug=True)
+    app.run(debug=True)
