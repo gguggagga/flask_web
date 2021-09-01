2f1aed85dc2533b4fd3f69112cf7d0ca4743b773
Your Name <you@example.com>
2021-09-01 16:49:22 +0900

    resgister2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import Counter
 from flask import Flask , render_template , redirect, request
 from data import Articles
 import pymysql
@@ -20,6 +21,22 @@ app = Flask(__name__)
 def index():
     name="KIM"
     return render_template('index.html',data=name)
+
+@app.route('/register', methods=['GET', 'POST'])
+def regitster():
+    if request.method =='GET':        
+        return render_template('register.html')
+    else:
+        username = request.form["username"]
+        email = request.form["email"]
+        password= request.form['password']
+        cosur = db_connection.cursor()
+        sql = f"INSERT INTO users (username, email, password) VALUES ('{username}', '{email}', '{password}');"
+        cosur.execute(sql)
+        db_connection.commit()
+        # return render_template('re_success.html')
+        return redirect('/')
+        
 
 @app.route('/articles', methods=['GET', 'POST'])
 def articles():
